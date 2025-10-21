b31b02dc3c40d7f4312351bba082c139c58a3f6a
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-20 23:47:07 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+
+const CACHE_NAME = 'notas-pwa-cache-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
