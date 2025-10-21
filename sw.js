5770b1b822cb24a77949faff4c901de132733d7b
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-21 00:56:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,24 @@
 
-const CACHE_NAME = 'notas-pwa-cache-v4';
+const CACHE_NAME = 'notas-pwa-cache-v5';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png',
   './apple-touch-icon.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
